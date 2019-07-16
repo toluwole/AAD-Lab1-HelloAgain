f1494799b2e7b90f1bf5e6289b2ada4c3c4bb3a8
Tope Oluwole <toluwole@aol.com>
2019-07-15 22:21:17 -0700

    Print text to UI.

--- app/src/main/java/com/example/helloagain/MainActivity.java
+++ app/src/main/java/com/example/helloagain/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.helloagain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
     }
 }
