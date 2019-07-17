79704902c5c986d3cdfdc26125ea97054cba6bcf
Tope Oluwole <toluwole@aol.com>
2019-07-16 21:06:58 -0700

    Use resources for app name and textview.

--- app/src/main/java/com/example/helloagain/MainActivity.java
+++ app/src/main/java/com/example/helloagain/MainActivity.java
@@ -3,15 +3,12 @@ package com.example.helloagain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, Android");
-        setContentView(tv);
+        setContentView(R.layout.activity_main);
     }
 }
